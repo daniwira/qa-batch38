32abb99cc7c18e6f0b479bb7019fe876804597fe
Dani Wira Sasmita <daniwira45@gmail.com>
2022-11-16 07:54:32 +0700

    add soal1

---
+++ soal1.js
@@ -0,0 +1,26 @@
+var x= 36;
+var angka=Math.sqrt(x) 
+if (x%2==0) 
+{
+   console.log("harus bilangan genap");
+   if (angka > 0)
+   {
+      console.log("input " + x + " hasil = " + angka);
+   }
+   else
+   {
+      console.log("Tidak bisa input bilangan negatif");
+   }
+}           
+else
+{
+   console.log("harus bilangan genap");
+   if (angka > 0)
+   {
+      console.log("Tidak bisa input bilangan ganjil" + " " +x);
+   }
+   else
+   {
+      console.log("Tidak bisa input bilangan negatif");
+   }
+}
